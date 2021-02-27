6117a85ac641f75e5450e093c8e01ec30ac214a5
hasnain <hasnainkhankqs@gmail.com>
2021-02-27 13:56:36 +0500

    customization done, added contact,about

--- app.py
+++ app.py
@@ -9,10 +9,15 @@ app.config.from_pyfile('config.py')
 def entry():
     return render_template('index.html' , active='index')
 
-@app.route('/product')
-def product():
-    return render_template('product.html')
+@app.route('/about')
+def about():
+    return render_template('about.html'  , active='about')
 
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html'  , active='contact')
 # home route
 @app.route("/home")
 def home():
