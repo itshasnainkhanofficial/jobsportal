45174bef86697acec76667eb3946d4ff4b1e348a
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2021-02-28 18:00:32 +0500

    Job type fetching is now asynchronus
    Added count parameter to limit no of jobs fetched in scrape method
    
    added method to load more jobs.

--- IndeedScrapper.py
+++ IndeedScrapper.py
@@ -1,6 +1,8 @@
+import asyncio
+
+import aiohttp as aiohttp
 import requests
-from bs4 import BeautifulSoup
-import time
+from bs4 import BeautifulSoup, Tag
 import re
 
 from Job import Job
@@ -11,24 +13,46 @@ class IndeedScrapper:
         self.jobTitle = jobTitle
         self.location = location
 
-    def scrape(self) -> list[Job]:
+        self.currentUrl = self.getUrl()
+        self.nextUrl = ''
+
+    @staticmethod
+    async def fetchJobType(job):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(job.jobLink) as response:
+                html = await response.text()
+                try:
+                    job.jobType = re.search("<p>(Job (T|t)ypes?.?(\w|\s|-|,)+)</p>", html).group(1)
+                except AttributeError:
+                    job.jobType = ''
+
+    def scrape(self, count=None) -> list[Job]:
         jobs: list[Job] = []
 
-        url = self.getUrl()
+        response = requests.get(self.currentUrl)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        rawJobs = soup.find_all('div', 'jobsearch-SerpJobCard', limit=count)
 
-        while True:
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            rawJobs = soup.find_all('div', 'jobsearch-SerpJobCard')
+        print('parsing')
+        for rawJob in rawJobs:
+            jobs.append(self.parseRawJob(rawJob))
+        print('parsing done')
 
-            for rawJob in rawJobs:
-                jobs.append(self.parseRawJob(rawJob))
+        #         time.sleep(4)
+        try:
+            self.nextUrl = 'https://pk.indeed.com' + soup.find('a', {'aria-label': 'Next'}).get('href')
+        except AttributeError:
+            self.nextUrl = ''
 
-            #         time.sleep(4)
-            # try:
-            #     url = 'https://pk.indeed.com' + soup.find('a', {'aria-label': 'Next'}).get('href')
-            # except AttributeError:
-            break
+        print('now fetching job types')
+        jobTypeTasks = []
+        for job in jobs:
+            jobTypeTasks.append(self.fetchJobType(job))
+
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.gather(*jobTypeTasks))
+        print('completed fetching job types')
 
         return jobs
 
@@ -36,33 +60,34 @@ class IndeedScrapper:
         baseUrl = "https://pk.indeed.com/jobs?q={}&l={}"
         return baseUrl.format(self.jobTitle, self.location)
 
-    def getDetailsUrl(self, company, location, jk):
+    @staticmethod
+    def getDetailsUrl(company, location, jk):
         detailsBaseUrl = "https://pk.indeed.com/viewjob?cmp={}&t={}&jk={}"
         return detailsBaseUrl.format(company, location, jk)
 
-    def parseRawJob(self, rawJob) -> Job:
+    def parseRawJob(self, rawJob: Tag) -> Job:
         title = rawJob.find('h2', 'title').a.get('title')
-        company = rawJob.find('span', 'company').text.strip()
-        location = rawJob.find('span', 'location').text.strip()
+        company = self.safeFindHtmlTag(rawJob, 'span', 'company')
+        location = self.safeFindHtmlTag(rawJob, 'span', 'location')
 
-        # this fetches value of data-jk property of the top level tag. It is used to construct a url for details of this job
+        # this fetches value of data-jk property of the top level tag. It is used to construct a url for details of
+        # this job
         jk = rawJob.get('data-jk')
         url = self.getDetailsUrl(company, location, jk)
-        textResponse = requests.get(url).text
-        try:
-            jobType = re.search("<p>(Job (T|t)ypes?.?(\w|\s|-|,)+)</p>", textResponse).group(1)
-        except AttributeError:
-            jobType = ''
 
-        try:
-            salary = rawJob.find('span', 'salaryText').text.strip()
-        except AttributeError:
-            salary = ''
-        try:
-            requirements = rawJob.find('div', 'jobCardReqItem').text.strip()
-        except AttributeError:
-            requirements = ''
+        salary = self.safeFindHtmlTag(rawJob, 'span', 'salaryText')
+        requirements = self.safeFindHtmlTag(rawJob, 'div', 'jobCardReqItem')
         summary = rawJob.find('div', 'summary').text.strip()
 
         return Job(title=title, company=company, location=location, salary=salary, requirement=requirements,
-                   summary=summary, jobType=jobType, jobLink=url, vaccancies='', lastDate='', contact='', address='')
+                   summary=summary, jobType='', jobLink=url, vaccancies='', lastDate='', contact='', address='')
+
+    # find the text within an html tag if it exists.
+    @staticmethod
+    def safeFindHtmlTag(job: Tag, tagToFind, className):
+        element = job.find(tagToFind, className)
+        return '' if element is None else element.text.strip()
+
+    def loadMore(self) -> list[Job]:
+        self.currentUrl = self.nextUrl
+        return self.scrape()
--- app.py
+++ app.py
@@ -6,18 +6,21 @@ from Job import Job
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+recentJobs: list[Job] = []
 jobs: list[Job] = []
 
 
 @app.route('/')
 def entry():
+    global recentJobs
     global jobs
-    if len(jobs) == 0:
-        indeedScrapper: IndeedScrapper = IndeedScrapper("Android Developer", "Karachi")
-        jobs = indeedScrapper.scrape()
-        print(len(jobs))
 
-    return render_template('index.html', active='index', jobs=jobs)
+    if len(recentJobs) == 0:
+        indeedScrapper: IndeedScrapper = IndeedScrapper("Android Developer", '')
+        recentJobs = indeedScrapper.scrape()
+        print(len(recentJobs))
+
+    return render_template('index.html', active='index', jobs=jobs, recentJobs=recentJobs)
 
 
 @app.route('/about')
